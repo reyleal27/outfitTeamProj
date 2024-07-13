4d468f3cd9e4c9610ed0ca229da42ef61dd66540
reyleal27 <reyvincentleal@gmail.com>
2024-07-13 22:42:34 +0800

    frontend outfit

--- backend/middlewares/index.js
+++
@@ -1,8 +0,0 @@
-const validateBody = require("./validateBody");
-const authenticate = require("./authenticate");
-//const authSocial = require("./authSocial");
-
-module.exports = {
-  validateBody,
-  authenticate,
- };
