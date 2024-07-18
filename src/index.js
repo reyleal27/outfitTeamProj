9c98073bed43eac95ad275abae90553c322a5fd4
reyleal27 <reyvincentleal@gmail.com>
2024-07-18 13:27:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/Team-Outfit">
+        <BrowserRouter basename="/outfitTeamProj">
           <GoogleOAuthProvider clientId="424051864171-nssd8rqgukkpunne874lqup4akl232s6.apps.googleusercontent.com">
             <ThemeProvider>
               <App />
